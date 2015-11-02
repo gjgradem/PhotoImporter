ed0554109d3a1dfe4b93209255ca138a67bc02d0
gjgradem <gjgradem@student.avans.nl>
2015-11-02 09:46:31 +0100

    Initial Push

---
+++ PhotoImporter/PhotoImporter/Model/UsbDevice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoImporter.Model
+{
+    public class UsbDevice
+    {
+        public string DeviceId { get; set; }
+        public string PnpDeviceId { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ PhotoImporter/PhotoImporter/ViewModel/UsbDeviceVM.cs
@@ -0,0 +1,63 @@
+﻿using GalaSoft.MvvmLight;
+using PhotoImporter.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoImporter.ViewModel
+{
+    class UsbDeviceVM : ViewModelBase
+    {
+        private UsbDevice _usbDevice;
+
+        public UsbDeviceVM()
+        {
+            _usbDevice = new UsbDevice();
+        }
+
+        public UsbDeviceVM(UsbDevice device)
+        {
+
+        }
+        public string DeviceId
+        {
+            get
+            {
+                return _usbDevice.DeviceId;  
+            }
+            set
+            {
+                _usbDevice.DeviceId = value;
+                RaisePropertyChanged("DeviceId");
+            }
+        }
+
+        public string PnpDeviceId
+        {
+            get
+            {
+                return _usbDevice.PnpDeviceId;
+            }
+            set
+            {
+                _usbDevice.PnpDeviceId = value;
+                RaisePropertyChanged("PnpDeviceId");
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return _usbDevice.Description;  
+            }
+            set
+            {
+                _usbDevice.Description = value;
+                RaisePropertyChanged("Description");
+            }
+        }
+    }
+}
